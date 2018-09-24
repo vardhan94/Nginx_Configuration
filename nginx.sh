61677651d0a561a2f418b4a8e991e78a62ccad41
Yash Kumar Shah <yashkumarshah22@gmail.com>
2018-09-24 15:42:53 +0530

    Cofig added

---
+++ nginx.sh
@@ -0,0 +1,30 @@
+export BUILD_DIR=#your-dir
+export NGINX_DIR=/etc/nginx
+export SBIN_DIR=/usr/sbin
+export PID_DIR=/var/run
+export LOCK_DIR=/var/run
+export LOG_DIR=/var/log/nginx
+export RUN_DIR=/var/run
+export CACHE_DIR=/var/cache
+cd ${BUILD_DIR}
+./configure \
+ --prefix=${NGINX_DIR} \
+ --sbin-path=${SBIN_DIR}/nginx \
+ --conf-path=${NGINX_DIR}/nginx.conf \
+ --pid-path=${PID_DIR}/nginx.pid \
+ --lock-path=${LOCK_DIR}/nginx.lock \
+ --error-log-path=${LOG_DIR}/error.log \
+ --http-log-path=${LOG_DIR}/access.log \
+ --http-client-body-temp-path=${CACHE_DIR}/client_body_temp \
+ --http-fastcgi-temp-path=${CACHE_DIR}/fastcgi_temp \
+ --with-http_gzip_static_module \
+ --with-http_stub_status_module \
+ --with-http_ssl_module \
+ --with-pcre \
+ --with-file-aio \
+ --with-http_realip_module \
+ --without-http_scgi_module \
+ --without-http_uwsgi_module \
+ --without-http_proxy_module \
+ --user=nginx \
+ --group=nginx
---
+++ nginx_pre_conf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#update your build essentials
+sudo apt-get install build-essential
+#update the libpcre3
+sudo apt-get install libpcre3
+#update its libraires tooo
+sudo apt-get install libpcre3-dev
+#install openssl
+sudo apt-get install openssl
+#install its dev libraries
+sudo apt-get install libssl-dev
+sudo apt-get install zlib1g zlib1g-dev
